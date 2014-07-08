73c35682aaa20cdc0a8005d05fe9028ef15e569b
Jeremagician <jeremagician@gmail.com>
2014-07-08 22:54:56 +0200

    project default

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+
+using namespace std;
+using namespace sf;
 
 int main(void)
 {
-	std::cout << "MiniLD 53 !" << std::endl;
+	RenderWindow window(VideoMode(1000,800), "MiniLD 53!");
+
+	b2Vec2 gravity(0.0f, -10.0f);
+	b2World world(gravity);
+
+	while(window.isOpen())
+	{
+		Event event;
+		while(window.pollEvent(event))
+		{
+			if(event.type == Event::Closed)
+				window.close();
+
+			window.clear();
+
+			window.display();
+		}
+
+	}
+
 	return EXIT_SUCCESS;
 };
