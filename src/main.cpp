5be7defbb8c566132953645d083e7fb4c54f5ce3
Jeremagician <jeremagician@gmail.com>
2014-07-10 20:18:37 +0200

    we don't use Box2D anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
-#include <Box2D/Box2D.h>
 
 using namespace std;
 using namespace sf;
@@ -10,22 +9,26 @@ int main(void)
 {
 	RenderWindow window(VideoMode(1000,800), "MiniLD 53!");
 
-	b2Vec2 gravity(0.0f, -10.0f);
-	b2World world(gravity);
-
 	while(window.isOpen())
 	{
 		Event event;
 		while(window.pollEvent(event))
 		{
-			if(event.type == Event::Closed)
+			switch(event.type)
+			{
+			case Event::Closed:
 				window.close();
-
-			window.clear();
-
-			window.display();
+				break;
+			case Event::KeyReleased:
+				if(event.key.code == Keyboard::Escape)
+					window.close();
+				break;
+			}
 		}
 
+		window.clear();
+
+		window.display();
 	}
 
 	return EXIT_SUCCESS;
