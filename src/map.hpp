2ec817813e81f93ac34e471470cca03773088ecf
Needs <needs@mailoo.org>
2014-07-13 01:04:02 +0200

    Introduce 'Tile' as a class, prepare for map layers
    Tile as a class is easier to modify with inheritance.  Since map layers
    can define new tiles, this inheritance will be usefull to extends
    existing tiles.
    
    Also, this reworks is needed for futur work on generic tile parsing.

--- src/map.hpp
+++ src/map.hpp
@@ -4,21 +4,52 @@
 #include <fstream>
 #include <vector>
 
-enum Tile {
-	TILE_NONE,
-	TILE_START,
-	TILE_EXIT,
-	TILE_PLAIN,
-	TILE_EARTH,
+enum class Direction {
+	TOP = (1 << 0),
+	RIGHT = (1 << 1),
+	BOTTOM = (1 << 2),
+	LEFT = (1 << 3),
 };
 
-class Map {
+enum class TileBox {
+	PLAIN,
+	EDGE,
+	CORNER,
+	CORNER_IN,
+};
+
+class Tile
+{
+protected:
+	TileBox   box;
+	Direction facing;
+	Direction rotation;
+
+	enum {
+		TILE_NONE,
+		TILE_EXIT,
+		TILE_START,
+		TILE_PLAIN,
+		TILE_EARTH,
+		TILE_LAST,
+	};
+
+	/* It would be better to have something like enum inheritance  */
+	int type;
+
+public:
+	Tile(void);
+	~Tile(void);
+
+	void operator<<(std::ifstream &stream);
+	void operator>>(std::ostream &stream);
+};
+
+class Map
+{
 private:
 	unsigned width, height;
-	std::vector<std::vector<enum Tile> > tiles;
-
-	enum Tile char_to_tile(char c);
-	char tile_to_char(enum Tile tile);
+	std::vector<std::vector<Tile> > tiles;
 public:
 	Map(void);
 	~Map(void);
